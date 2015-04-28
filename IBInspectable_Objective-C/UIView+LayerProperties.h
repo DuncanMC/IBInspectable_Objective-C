db3a7be5762e9cc329828b66ee01c63423f01f90
Duncan Champney <DuncanC@aol.com>
2015-04-27 20:40:51 -0400

    Added .gitignore

---
+++ IBInspectable_Objective-C/UIView+LayerProperties.h
@@ -0,0 +1,13 @@
+//
+//  NSView+LayerProperties.h
+//  IBInspectable_Objective-C
+//
+//  Created by Duncan Champney on 4/27/15.
+//  Copyright (c) 2015 Duncan Champney. All rights reserved.
+//
+
+
+
+@interface NSView (LayerProperties)
+
+@end
---
+++ IBInspectable_Objective-C/UIView+LayerProperties.m
@@ -0,0 +1,13 @@
+//
+//  NSView+LayerProperties.m
+//  IBInspectable_Objective-C
+//
+//  Created by Duncan Champney on 4/27/15.
+//  Copyright (c) 2015 Duncan Champney. All rights reserved.
+//
+
+#import "NSView+LayerProperties.h"
+
+@implementation NSView (LayerProperties)
+
+@end
